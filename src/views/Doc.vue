2c2fc8030a0589aa7498afcabab531b1bc0880b8
lv <lvxiaoyanhaha@gmail.com>
2022-03-08 22:59:11 +0800

    feat(doc): 初始化左侧菜单栏布局

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,23 @@
   <div>
     <Topnav />
     <div class="content">
-      <aside>侧标菜单栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <h2>练手 UI 框架</h2>
       <p class="actions">
         <a href>Github</a>
-        <a href>开始</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
