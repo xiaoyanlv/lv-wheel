5d82800250c73ea8094532f88499d33f14b91d96
lv <lvxiaoyanhaha@gmail.com>
2022-03-08 22:39:19 +0800

    feat(home): 初始化 Home 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <div class="banner">
       <h1>lv UI</h1>
       <h2>练手 UI 框架</h2>
-      <p class="action">
+      <p class="actions">
         <a href>Github</a>
         <a href>开始</a>
       </p>
@@ -15,3 +15,26 @@
 <script setup lang="ts">
 import Topnav from '../components/Topnav.vue'
 </script>
+
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      height: 28px;
+      line-height: 28px;
+      border-radius: 14px;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
